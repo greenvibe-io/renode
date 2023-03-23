0266f71179fd98d0ad42522636132c2935488c65
Alon Levy <alon@greenvibe.io>
2023-03-23 06:27:53 +0200

    test_cc1352: automate warning collection

--- test_cc1352.sh
+++ test_cc1352.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-renode --console --disable-xwt test_cc1352.rscr
+
+function onexit {
+        #kill $RENODE
+        killall mono
+        cat test_output.txt | grep -i warn > ./port.warnings.txt
+}
+
+trap onexit EXIT
+
+renode --console --disable-xwt test_cc1352.rscr | tee test_output.txt &
+# TODO: find the name of the variable holding the last executed process background PID
+#RENODE=$$
+sleep 40
